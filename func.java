6b632d9a4c6f74134c514429028f1c3c58ee1432
Peter Sjöberg <p-sjoberg@hotmail.com>
2024-01-12 00:17:11 +0100

    Skapat func.java med en deathcounter som kan kallas från alla filer med func.incrementDeathcounter(); Detta ökar antal ggr spelaren dött med + 1

---
+++ func.java
@@ -0,0 +1,54 @@
+import java.io.*;
+
+public class func {
+
+    // Sökvägen till filen "statistics.json" lagras
+    private static final String JSON_FILE_PATH = "statistics.json";
+    // Strängen som ska stå i den filen lagras
+    private static final String COUNTER_KEY = "Deathcounter";
+
+    public static void incrementDeathcounter() throws IOException {
+        // Hämta det aktuella räknarvärdet från filen
+        int currentCount = readJsonFromFile();
+        
+        // Öka räknaren med ett
+        int newCount = currentCount + 1;
+        
+        // Spara det nya räknarvärdet till filen
+        writeJsonToFile(newCount);
+    }
+    
+    //Här kontrolleras om filen som anges på rad 5 existerar
+    private static int readJsonFromFile() throws IOException {
+        File file = new File(JSON_FILE_PATH);
+        
+        //Finns inte filen så anropas metoden writeJsonToFile som skapar filen
+        if (!file.exists()) {
+            writeJsonToFile(0);
+        }
+        
+        // Skapar Bufferedreader och öppnar filen
+        try (BufferedReader reader = new BufferedReader(new FileReader(JSON_FILE_PATH))) {
+
+            // Läser den första raden från filen och sparar den i variabeln line
+            String line = reader.readLine();
+
+            //kontrollerar om line då är null
+            if (line != null) {
+                // splittar strängen till innan kolontecknet = String och efter kolontecknet = Int
+                String[] parts = line.split(":");
+                return Integer.parseInt(parts[1].trim());
+            }
+        }
+        
+        return 0;
+    }
+    // skapar en BufferedWriter som öppnar filen för att skriva
+    private static void writeJsonToFile(int value) throws IOException {
+        // Anger den lagrade filvägen till statistics.json
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(JSON_FILE_PATH))) {
+            // Skriver den lagrade stränger + : + (Nya dödsantalet)
+            writer.write(COUNTER_KEY + ": " + value);
+        }
+    }
+}
