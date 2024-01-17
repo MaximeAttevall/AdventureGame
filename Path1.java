f6b2ecf1ebb00d7da957d13cf7773798076e495e
Maxime Attevall <max.attevall@gmail.com>
2024-01-17 20:03:11 +0100

    First commit in Path1 Branch - Maxime

--- Path1.java
+++ Path1.java
@@ -8,15 +8,20 @@ import java.io.File;
 import java.io.IOException;
 
 public class Path1 {
-    private static final String DEFAULT_BACKGROUND_PATH = "images/win.png";
-    private static final String OPTION1_BACKGROUND_PATH = "images/youdied.jpg";
+
+    private static final String WINNING_BACKGROUND_PATH = "images/win.png";
+    private static final String END_BACKGROUND_PATH = "images/youdied.jpg";
     private static final String OPTION2_BACKGROUND_PATH = "images/TwoPaths.png";
-    private static String currentBackgroundPath = DEFAULT_BACKGROUND_PATH;
+    private static final String OPTION1A_BACKGROUND_PATH = "images/Option1A.jpg";
+    private static final String OPTION1B_BACKGROUND_PATH = "images/Option1B.png";
+    private static final String OPTION1A1_BACKGROUND_PATH = "images/Deer.jpg";
+    private static final String EXIT_BACKGROUND_PATH = "images/exit.png";
+
+    private static String currentBackgroundPath = WINNING_BACKGROUND_PATH;
     private static JFrame frame;
 
     public static void main(String[] args) {
-        // Set the initial background to DEFAULT_BACKGROUND_PATH
-        currentBackgroundPath = DEFAULT_BACKGROUND_PATH;
+        currentBackgroundPath = WINNING_BACKGROUND_PATH;
 
         SwingUtilities.invokeLater(() -> {
             frame = createFrame();
@@ -53,39 +58,98 @@ public class Path1 {
         currentBackgroundPath = OPTION2_BACKGROUND_PATH;
         updateBackground();
 
-        // Now show the game options
-        showGameOptionsWithButtons();
+        showOptions("You see two paths in front of you, choose your path!",
+                "Option 1", "Option 2",
+                Path1::ToScene1, Path1::ToScene2);
     }
 
-    private static void showGameOptionsWithButtons() {
+    private static void showOptions(String message, String option1Text, String option2Text,
+                                    ActionListener option1Listener, ActionListener option2Listener) {
         JPanel buttonPanel = new JPanel();
         buttonPanel.setLayout(new FlowLayout());
 
-        JButton option1Button = new JButton("Option 1");
-        JButton option2Button = new JButton("Option 2");
+        JLabel messageLabel = new JLabel(message);
+        JButton option1Button = new JButton(option1Text);
+        JButton option2Button = new JButton(option2Text);
 
-        option1Button.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                JOptionPane.showMessageDialog(frame, "You Live!");
-                currentBackgroundPath = DEFAULT_BACKGROUND_PATH;
-                updateBackground();
-            }
-        });
+        option1Button.addActionListener(option1Listener);
+        option2Button.addActionListener(option2Listener);
 
-        option2Button.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                currentBackgroundPath = OPTION1_BACKGROUND_PATH;
-                updateBackground();
-            }
-        });
-
-        buttonPanel.add(option1Button); //Lägger till knapparna till button panelen
+        buttonPanel.add(messageLabel);
+        buttonPanel.add(option1Button);
         buttonPanel.add(option2Button);
 
-        frame.add(buttonPanel, BorderLayout.SOUTH); //Lägger till button panelen till vår JFrame
-        frame.revalidate(); //Uppdaterar våran JFrame med alla ny komponenter
+        frame.add(buttonPanel, BorderLayout.SOUTH);
+        frame.revalidate();
+    }
+
+    private static void ToScene1(ActionEvent e) {
+        currentBackgroundPath = OPTION1A_BACKGROUND_PATH;
+        updateBackground();
+
+        showOptions("You see a lost person in the woods, would you like to help them?",
+                "Yes", "No",
+                Path1::ToScene3, Path1::ToScene4);
+        
+    }
+
+    private static void ToScene2(ActionEvent e) {
+        currentBackgroundPath = END_BACKGROUND_PATH;
+        updateBackground();
+
+    }
+    private static void ToScene3(ActionEvent e) {
+        JOptionPane.showMessageDialog(frame, "You decided to help the person, the person will now follow along with you through the woods");
+
+        currentBackgroundPath = OPTION1A1_BACKGROUND_PATH;
+        updateBackground();
+
+        showOptions("You see a strange Deer infront of you, do you approach the deer?",
+                "Yes", "No",
+                Path1::ToScene6, Path1::ToScene4);
+
+    }
+
+    private static void ToScene4(ActionEvent e) {
+        JOptionPane.showMessageDialog(frame, "You ignore the person and keep going through the woods");
+
+        currentBackgroundPath = OPTION1A1_BACKGROUND_PATH;
+        updateBackground();
+
+        showOptions("You see a strange Deer infront of you, do you approach the deer?",
+                "Yes", "No",
+                Path1::ToScene2, Path1::ToScene5);
+    }
+
+    private static void ToScene5(ActionEvent e) {
+        JOptionPane.showMessageDialog(frame, "You ignore the deer and keep on going");
+
+        currentBackgroundPath = EXIT_BACKGROUND_PATH;
+        updateBackground();
+
+        showOptions("You see a exit from the woods, do you take it?",
+                "Yes", "No",
+                Path1::ToScene7, Path1::ToScene2);
+    }
+
+    private static void ToScene6(ActionEvent e) {
+        JOptionPane.showMessageDialog(frame, "The deer attacks the boy and you manage to escape on your own");
+        JOptionPane.showMessageDialog(frame, "You keep going through the woods");
+
+        currentBackgroundPath = EXIT_BACKGROUND_PATH;
+        updateBackground();
+
+        showOptions("You see a exit from the woods, do you take it?",
+                "Yes", "No",
+                Path1::ToScene7, Path1::ToScene2);
+    }
+    private static void ToScene7(ActionEvent e) {
+        JOptionPane.showMessageDialog(frame, "You exit the woods!");
+
+        currentBackgroundPath = WINNING_BACKGROUND_PATH;
+        updateBackground();
+
+        JOptionPane.showMessageDialog(frame, "Congratulations you see the light and have exited the woods!");
     }
 
     private static void updateBackground() {
